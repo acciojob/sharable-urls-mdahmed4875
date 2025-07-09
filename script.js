d139bd5dc6b06529b3cfa2c1cf4baf24f172251d
mdahmed4875 <140884439+mdahmed4875@users.noreply.github.com>
2025-07-09 09:45:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-// your code here
+let btn=document.getElementById("btn");
+btn.addEventListener('click',(e)=>{
+	 e.preventDefault();
+	let name=document.getElementById("name");
+	let year=document.getElementById("year");
+	let url=document.getElementById("url");
+	url.innerText = `https://localhost:8080/?name=${name.value}&year=${year.value}`;
+})
